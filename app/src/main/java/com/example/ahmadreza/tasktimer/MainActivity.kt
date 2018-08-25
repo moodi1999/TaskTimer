33517b7130d29e813aaa4812d1ce750c81a6354b
ahmadreza <moodi1378@gmail.com>
2018-08-25 13:46:16 +0430

    added recycler view in fragment_main.xml and make the recycler item view in task_list_item.xml

--- app/src/main/java/com/example/ahmadreza/tasktimer/MainActivity.kt
+++ app/src/main/java/com/example/ahmadreza/tasktimer/MainActivity.kt
@@ -91,7 +91,7 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.menumain_setting -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
